5c9014e2a4990845c4b3bcb93fd3a59ea7c72087
John Mataa <johnnymataa019@gmail.com>
2025-11-25 15:22:59 +0200

    updating the dashboard

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -59,7 +59,7 @@ export default function Login() {
                   type={showPassword ? 'text' : 'password'}
                   autoComplete="current-password"
                   required
-                  className="block w-full appearance-none rounded-lg border border-gray-300 px-3 py-2 pr-10 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                  className="block w-full appearance-none rounded-0g border border-gray-300 px-3 py-2 pr-10 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
                   placeholder="Enter your password"
                 />
                 <button
