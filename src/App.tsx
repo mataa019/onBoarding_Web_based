9a549c7e3cf5285e7f1e684f9554ce623c9ecbc5
John Mataa <johnnymataa019@gmail.com>
2025-11-25 15:01:58 +0200

    updating the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Layout } from './components/layout'
 import Dashboard from './pages/Dashboard'
+import Login from './pages/Auth/Login'
 
 function App() {
   return (
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Login/>} />
+          <Route path="/Dashboard " element={<div>Analytics Page Coming Soon...</div>} />
           <Route path="/documents" element={<div>Documents Page Coming Soon...</div>} />
           <Route path="/projects" element={<div>Projects Page Coming Soon...</div>} />
           <Route path="/notifications" element={<div>Notifications Page Coming Soon...</div>} />
